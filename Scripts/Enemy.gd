01415220c5911b33301627287a6d7af60ac0fd80
Tyler-Osborne <tyler.dean.osborne@gmail.com>
2021-03-13 11:57:35 -0600

    Current Dev Branch. Have most mechanics finished. Player dies when touched and enemies die when shot

---
+++ Scripts/Enemy.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+const MOVE_SPEED : int = 150
+
+onready var raycast : RayCast2D = $RayCast2D
+
+var player = null
+
+func _ready():
+	add_to_group("enemies")
+
+func _physics_process(delta):
+	if player == null:
+		return
+	
+	var vector_to_player : Vector2 = player.global_position - global_position
+	vector_to_player = vector_to_player.normalized()
+	global_rotation = atan2(vector_to_player.y, vector_to_player.x)
+	move_and_collide(vector_to_player * MOVE_SPEED * delta)
+
+	if raycast.is_colliding():
+		var colliding_object = raycast.get_collider()
+		if colliding_object.name == "Player":
+			colliding_object.kill()
+
+func kill():
+	queue_free()
+
+func set_player(p):
+	player = p
---
+++ Scripts/Player.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+const MOVE_SPEED : int = 200
+
+onready var raycast = $RayCast2D
+
+func _ready():
+    yield(get_tree(), "idle_frame")
+    get_tree().call_group("enemies", "set_player", self)
+
+func _physics_process(delta):
+    var move_vector : Vector2 = Vector2()
+
+    if Input.is_action_pressed("move_up"):
+        move_vector.y -= 1
+    if Input.is_action_pressed("move_down"):
+        move_vector.y += 1
+    if Input.is_action_pressed("move_left"):
+        move_vector.x -= 1
+    if Input.is_action_pressed("move_right"):
+        move_vector.x += 1
+    move_vector = move_vector.normalized()
+    move_and_collide(move_vector * MOVE_SPEED * delta)
+    position = Vector2(clamp(position.x, 0, get_viewport_rect().size.x), \
+        clamp(position.y, 0, get_viewport_rect().size.y))
+
+    var look_vector = get_global_mouse_position() - global_position
+    global_rotation = atan2(look_vector.y, look_vector.x)
+
+    if Input.is_action_just_pressed("shoot"):
+        var colliding_object = raycast.get_collider()
+        if raycast.is_colliding() and colliding_object.has_method("kill"):
+            colliding_object.kill()
+
+func kill():
+    get_tree().reload_current_scene()
